31000b5dfa82cc9b378df385aa8d8d1a8c68fb7f
Knio <iamknio@outlook.com>
2022-01-19 17:16:29 +0800

    更新从配置文件导入client、url

--- src/main/java/com/example/demo/controller/AouthrizeConteoller.java
+++ src/main/java/com/example/demo/controller/AouthrizeConteoller.java
@@ -5,6 +5,7 @@ import com.example.demo.dto.giteeuser;
 import com.example.demo.provider.GiteeProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,21 @@ public class AouthrizeConteoller {
 
     @Autowired
     private GiteeProvider giteeProvider;
+
+    @Value("${gitee.client.id}")
+    private String clientId;
+    @Value("${gitee.client.secret}")
+    private String clientSecret;
+    @Value("${gitee.redirect.url}")
+    private String redirectUrl;
     
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
-        accessTokenDto.setClient_id("3af92bdef23405b4bf970b5dfbc76ff05d71e65a6a38bacdf2ff561533f8008d");
-        accessTokenDto.setClient_sceret("7f3d1e07dee8e8324992ef4659ab50667ca1f3434b5434dc17fbae8fb37484ad");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_sceret(clientSecret);
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:80/callback");
+        accessTokenDto.setRedirect_uri(redirectUrl);
         String accessToken = giteeProvider.getAccessToken(accessTokenDto);
         giteeuser user = giteeProvider.getUser(accessToken);
         System.out.println(user.getName());
